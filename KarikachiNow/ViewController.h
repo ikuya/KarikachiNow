614c59e4d47be211ab9a913869cc48525dd62650
ikuya <ikuya@ssag-mbp-iossec.local>
2014-10-17 11:30:01 +0900

    Initial Commit

---
+++ KarikachiNow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KarikachiNow
+//
+//  Created by Ikuya Fukumoto on 10/17/14.
+//  Copyright (c) 2014 if. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
